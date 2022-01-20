441a565dabfbc759e5de70ff69acd119c98a7c46
Nuno Ribeiro <nmpribeiro@gmail.com>
2022-01-20 10:08:44 +0100

    Sets up messaging between peers

--- libs/p2p_server/src/lib.rs
+++ libs/p2p_server/src/lib.rs
@@ -1,12 +1,21 @@
 use log::info;
 
-// https://github.com/libp2p/rust-libp2p/blob/master/src/tutorial.rs
-use futures::prelude::*;
-use libp2p::multiaddr::multiaddr;
-use libp2p::ping::{Ping, PingConfig};
-use libp2p::swarm::{Swarm, SwarmEvent};
-use libp2p::{identity, PeerId};
+use async_std::{io, task};
+use futures::{
+    prelude::{stream::StreamExt, *},
+    select,
+};
+use libp2p::{
+    floodsub::{self, Floodsub, FloodsubEvent},
+    identity,
+    mdns::{Mdns, MdnsConfig, MdnsEvent},
+    multiaddr::multiaddr,
+    swarm::SwarmEvent,
+    NetworkBehaviour, PeerId, Swarm,
+};
 use std::error::Error;
+
+// https://github.com/libp2p/rust-libp2p/blob/master/src/tutorial.rs
 
 async fn listen(port: u16, peers: Vec<&str>) -> Result<(), Box<dyn Error>> {
     // Setup logging
@@ -14,27 +23,69 @@ async fn listen(port: u16, peers: Vec<&str>) -> Result<(), Box<dyn Error>> {
 
     // Get address of this peer
     let my_addr = &format!("localhost:{}", &port);
-    info!("Setting up {}", my_addr);
+    println!("Setting up {}", my_addr);
 
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
-    info!("Local peer id: {:?}", local_peer_id);
+    println!("Local peer id: {:?}", local_peer_id);
 
     let transport = libp2p::development_transport(local_key).await?;
+
+    // Create a Floodsub topic
+    let floodsub_topic = floodsub::Topic::new("chat");
+
+    // We create a custom network behaviour that combines floodsub and mDNS.
+    // In the future, we want to improve libp2p to make this easier to do.
+    // Use the derive to generate delegating NetworkBehaviour impl and require the
+    // NetworkBehaviourEventProcess implementations below.
+    #[derive(NetworkBehaviour)]
+    #[behaviour(out_event = "OutEvent")]
+    struct MyBehaviour {
+        floodsub: Floodsub,
+        mdns: Mdns,
+
+        // Struct fields which do not implement NetworkBehaviour need to be ignored
+        #[behaviour(ignore)]
+        #[allow(dead_code)]
+        ignored_member: bool,
+    }
+
+    #[derive(Debug)]
+    enum OutEvent {
+        Floodsub(FloodsubEvent),
+        Mdns(MdnsEvent),
+    }
+
+    impl From<MdnsEvent> for OutEvent {
+        fn from(v: MdnsEvent) -> Self {
+            Self::Mdns(v)
+        }
+    }
+
+    impl From<FloodsubEvent> for OutEvent {
+        fn from(v: FloodsubEvent) -> Self {
+            Self::Floodsub(v)
+        }
+    }
 
     // Create a ping network behaviour.
     //
     // For illustrative purposes, the ping protocol is configured to
     // keep the connection alive, so a continuous sequence of pings
     // can be observed.
-    let behaviour = Ping::new(PingConfig::new().with_keep_alive(true));
+    // let behaviour = Ping::new(PingConfig::new().with_keep_alive(true));
 
-    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
+    let mut swarm = {
+        let mdns = task::block_on(Mdns::new(MdnsConfig::default()))?;
+        let mut behaviour = MyBehaviour {
+            floodsub: Floodsub::new(local_peer_id),
+            mdns,
+            ignored_member: false,
+        };
 
-    // Tell the swarm to listen on all interfaces and a random, OS-assigned
-    // port.
-    let my_addr = format!("/ip4/0.0.0.0/tcp/{}", port);
-    swarm.listen_on(my_addr.parse()?)?;
+        behaviour.floodsub.subscribe(floodsub_topic.clone());
+        Swarm::new(transport, behaviour, local_peer_id)
+    };
 
     // Dial the peer identified by the multi-address given as the second
     // command-line argument, if any.
@@ -52,11 +103,57 @@ async fn listen(port: u16, peers: Vec<&str>) -> Result<(), Box<dyn Error>> {
         println!("Dialed {}", peer)
     }
 
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    let my_addr = format!("/ip4/0.0.0.0/tcp/{}", port);
+    swarm.listen_on(my_addr.parse()?)?;
+
     loop {
-        match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {:?}", address),
-            SwarmEvent::Behaviour(event) => println!("{:?}", event),
-            _ => {}
+        select! {
+            line = stdin.select_next_some() => swarm
+                .behaviour_mut()
+                .floodsub
+                .publish(floodsub_topic.clone(), line.expect("Stdin not to close").as_bytes()),
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    println!("Listening on {:?}", address);
+                }
+                SwarmEvent::Behaviour(OutEvent::Floodsub(
+                    FloodsubEvent::Message(message)
+                )) => {
+                    println!(
+                        "Received: '{:?}' from {:?}",
+                        String::from_utf8_lossy(&message.data),
+                        message.source
+                    );
+                }
+                SwarmEvent::Behaviour(OutEvent::Mdns(
+                    MdnsEvent::Discovered(list)
+                )) => {
+                    for (peer, _) in list {
+                        swarm
+                            .behaviour_mut()
+                            .floodsub
+                            .add_node_to_partial_view(peer);
+                    }
+                }
+                SwarmEvent::Behaviour(OutEvent::Mdns(MdnsEvent::Expired(
+                    list
+                ))) => {
+                    for (peer, _) in list {
+                        if !swarm.behaviour_mut().mdns.has_node(&peer) {
+                            swarm
+                                .behaviour_mut()
+                                .floodsub
+                                .remove_node_from_partial_view(&peer);
+                        }
+                    }
+                },
+                _ => {}
+            }
         }
     }
 
